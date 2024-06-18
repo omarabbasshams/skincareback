0f3bb5ad9e34a79548748d1a390421a5bf007056
omarabbasshams <omarabbasshams@gmail.com>
2024-06-18 06:14:37 +0300

    edit pridiction table

--- database/migrations/2024_06_12_002555_create_predictions_table.php
+++ database/migrations/2024_06_12_002555_create_predictions_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('predictions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('image_path');
+            $table->string('prediction_result');
             $table->timestamps();
         });
     }
